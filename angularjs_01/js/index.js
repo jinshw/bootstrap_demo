4a969560e27de2b8625d3c0d60f0257ac1cb31b9
jinshw <jinshengwang2007@163.com>
2015-06-23 23:11:13 +0800

    angularjs directive scope

--- angularjs_01/js/index.js
+++ angularjs_01/js/index.js
@@ -191,5 +191,22 @@ myApp.controller("TestFormModule", ["$scope",function ($scope) {
     }
 }]);
 
+function Controller($scope) {
+    $scope.master = {};
+
+    $scope.update = function(user) {
+        $scope.master = angular.copy(user);
+    };
+
+    $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+    };
+
+    $scope.isUnchanged = function(user) {
+        return angular.equals(user, $scope.master);
+    };
+
+    $scope.reset();
+}
 
 
