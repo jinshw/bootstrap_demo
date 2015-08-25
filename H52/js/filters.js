daa7bd9d2b816660c49bf8b65cc5b6966847e286
jinshw <jinshengwang2007@163.com>
2015-08-25 23:46:33 +0800

    angular + requirjs

---
+++ H52/js/filters.js
@@ -0,0 +1,15 @@
+/**
+ * Created by JINSHW on 2015-08-25.
+ */
+define(['angular', 'services'], function (angular, services) {
+    'use strict';
+
+    /* Filters */
+
+    angular.module('myApp.filters', ['myApp.services'])
+        .filter('interpolate', ['version', function(version) {
+            return function(text) {
+                return String(text).replace(/\%VERSION\%/mg, version);
+            };
+        }]);
+});
