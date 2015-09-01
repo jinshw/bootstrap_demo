0a2c781fdcafe9b106d9647278c6d520a4b1b055
jinshw <jinshengwang2007@163.com>
2015-09-01 23:17:13 +0800

    echarts

---
+++ H53/js/echartsdemo.js
@@ -0,0 +1,52 @@
+/**
+ * Created by JINSHW on 2015-09-01.
+ */
+define( [
+        'echarts',
+        'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
+    ],
+    function (ec) {
+        'use strict';
+         return   function(){
+
+
+            // 基于准备好的dom，初始化echarts图表
+            var myChart = ec.init(document.getElementById('bar'));
+
+            var option = {
+                tooltip: {
+                    show: true
+                },
+                legend: {
+                    data:['销量','产量']
+                },
+                xAxis : [
+                    {
+                        type : 'category',
+                        data : ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","手巾"]
+                    }
+                ],
+                yAxis : [
+                    {
+                        type : 'value'
+                    }
+                ],
+                series : [
+                    {
+                        "name":"销量",
+                        "type":"bar",
+                        "data":[5, 20, 40, 10, 10, 20]
+                    },
+                    {
+                        "name":"产量",
+                        "type":"bar",
+                        "data":[5, 20, 40, 10, 10, 20]
+                    }
+                ]
+            };
+
+            // 为echarts对象加载数据
+            myChart.setOption(option);
+        }
+    }
+);
--- H53/js/main.js
+++ H53/js/main.js
@@ -7,7 +7,9 @@ require.config({
         angularUiRoute: '../../lib/angular-1.2.20/angular-ui-router',
         angularRoute: '../../lib/angular-1.2.20/angular-route',
         angularMocks: '../../lib/angular-1.2.20/angular-mocks',
-        text: '../../bower_components/requirejs-text/text'
+        text: '../../bower_components/requirejs-text/text',
+        echarts:'../../lib/echarts-2.2.7/build/dist/echarts',
+"echarts/chart/bar":'../../lib/echarts-2.2.7/build/dist/chart/bar'
     },
     shim: {
         'angular' : {'exports' : 'angular'},
@@ -32,7 +34,8 @@ window.name = "NG_DEFER_BOOTSTRAP!";
 require( [
     'angular',
     'app',
-    'routes'
+    'routes',
+    'echartsdemo'
 ], function(angular, app, routes) {
     'use strict';
     var $html = angular.element(document.getElementsByTagName('html')[0]);
@@ -40,4 +43,7 @@ require( [
     angular.element().ready(function() {
         angular.resumeBootstrap([app['name']]);
     });
+
+
+
 });
