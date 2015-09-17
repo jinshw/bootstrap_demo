1a02a7dd4876c4e71fdad825372d6c42b83e2d95
jinshw <jinshengwang2007@163.com>
2015-09-17 22:06:25 +0800

    echarts

--- H54/js/echartsdemo/bardemo.js
+++ H54/js/echartsdemo/bardemo.js
@@ -8,53 +8,55 @@ define( [
         'use strict';
 
            //柱图
-           var barTU = function (obj){
+           var barTU = function (obj,option){
             // 基于准备好的dom，初始化echarts图表
 //            var myChart = echarts.init(document.getElementById('bar'));
             var myChart = echarts.init(obj);
 
-            var option = {
-                backgroundColor:'#FFFFFF',
-                tooltip: {
-                    show: true
-                },
-                legend: {
-                    data:['销量','产量']
-                },
-                toolbox: {
-                    show : true,
-                    feature : {
-                        mark : {show: true},
-                        dataView : {show: true, readOnly: false},
-                        magicType : {show: true, type: ['line', 'bar']},
-                        restore : {show: true},
-                        saveAsImage : {show: true}
-                    }
-                },
-                xAxis : [
-                    {
-                        type : 'category',
-                        data : ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","手巾"]
-                    }
-                ],
-                yAxis : [
-                    {
-                        type : 'value'
-                    }
-                ],
-                series : [
-                    {
-                        "name":"销量",
-                        "type":"bar",
-                        "data":[5, 20, 40, 10, 10, 20]
+            if(option == "undefined" || option == null){
+                option = {
+                    backgroundColor:'#FFFFFF',
+                    tooltip: {
+                        show: true
                     },
-                    {
-                        "name":"产量",
-                        "type":"bar",
-                        "data":[5, 20, 40, 10, 10, 20]
-                    }
-                ]
-            };
+                    legend: {
+                        data:['销量','产量']
+                    },
+                    toolbox: {
+                        show : true,
+                        feature : {
+                            mark : {show: true},
+                            dataView : {show: true, readOnly: false},
+                            magicType : {show: true, type: ['line', 'bar']},
+                            restore : {show: true},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    xAxis : [
+                        {
+                            type : 'category',
+                            data : ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","手巾"]
+                        }
+                    ],
+                    yAxis : [
+                        {
+                            type : 'value'
+                        }
+                    ],
+                    series : [
+                        {
+                            "name":"销量",
+                            "type":"bar",
+                            "data":[5, 20, 40, 10, 10, 20]
+                        },
+                        {
+                            "name":"产量",
+                            "type":"bar",
+                            "data":[5, 20, 40, 10, 10, 20]
+                        }
+                    ]
+                };
+            }
 
             // 为echarts对象加载数据
             myChart.setOption(option);
--- H54/js/main.js
+++ H54/js/main.js
@@ -33,11 +33,10 @@ require.config({
 require( [
     'jquery',
     'bardemo',
-    'bootstrap',
-    'handsontable'
+    'handsontable',
+    'echarts'
 ], function($,bardemo,handsontable) {//在function（）中echarts不能传递参数，因为echarts不符合AMD规范
     'use strict';
-
     var bar = document.getElementById('bar');
     var line = document.getElementById('line');
     var pie = document.getElementById('pie');
@@ -84,11 +83,17 @@ require( [
 
     hot = new Handsontable(container, {
         data:dataArr,
-                        minRows:20,
-                        minCols:12,
+        minRows:20,
+        minCols:12,
 //                        height: 396,
         width: 660,
         height: 800,
+
+       /* afterChange:function(changs,source){
+            console.log("changs=="+changs);
+            console.log("source=="+source);
+//            flashData(hot);
+        },*/
 
         colHeaders: true,
         rowHeaders: true,
@@ -97,12 +102,183 @@ require( [
         contextMenu: true
     });
 
+    hot.addHook('afterChange',function(changs,source){
+        console.log("changs=="+changs);
+        console.log("source=="+source);
+            flashData(hot);
+    })
     ar = hot.getData();
-    console.log(ar);
+    console.log("aaaaaaaaaaaa");
+
+
+//刷新按钮
+    function flashData(hot){
+        console.log("bbbbbbbbbbbbbb");
+        //获取表格二维数组
+        /* var dataArr = hot.getData();
+         console.log(dataArr);
+
+         //获取最大下标
+         var tempArr;
+         var maxI = 0;
+         var maxJ = 0;
+         for(var i=0;i<dataArr.length;i++){
+         tempArr = dataArr[i];
+         for(var j=0;j<tempArr.length;j++){
+         if(dataArr[i][j] == null){
+
+         }else{
+         if(i > maxI){
+         maxI = i;
+         }
+         if(j > maxJ){
+         maxJ = j;
+         }
+         }
+         }
+
+         }
+
+         console.log(hot.getData(maxI,maxJ));*/
+
+        var tempObj = getMaxIndex(hot.getData());
+        var maxI = tempObj.maxI;
+        var maxJ = tempObj.maxJ;
+
+
+        var dataCol_1 =hot.getDataAtCol(0);
+//                        var cols_1 = dataCol_1.slice(1,dataCol_1.length);
+        var cols_1 = dataCol_1.slice(1,maxI+1);
+        var cols_11 = cols_1.map(function(x){
+            if(x == null){
+                return "-";
+            }else{
+                return x;
+            }
+        });
+
+        var dataRow_1 = hot.getDataAtRow(0);
+//                        var row_1 = dataRow_1.slice(1,dataRow_1.length);
+        var row_1 = dataRow_1.slice(1,maxJ+1);
+        var row_11 = row_1.map(function(x){
+            if(x == null){
+                return "-";
+            }else{
+                return x;
+            }
+        });
+
+        var series = new Array();
+        var temp;
+        var colsTemp;
+        for(var i=1;i<dataArr[0].length;i++){
+            temp = new Object();
+            colsTemp = hot.getDataAtCol(i);
+            if(colsTemp[0] == null || colsTemp[0] == undefined){
+                temp.name = "jinshw";
+            }else{
+                temp.name = colsTemp[0];
+            }
+
+            temp.type="bar";
+            if(hot.isEmptyCol(i)){//列为空
+                temp.data =  colsTemp.slice(1,colsTemp.length);
+            }else{
+                temp.data =  colsTemp.slice(1,colsTemp.length);
+            }
+            temp.data = temp.data.map(function(x){
+                if(x == null){
+                    return "0";
+                }else{
+                    return x;
+                }
+            });
+
+            series.push(temp);
+        }
+        console.log(row_11);
+        console.log(series);
+
+        var option = {
+            tooltip: {
+                show: true
+            },
+            legend: {
+                data:row_11
+//                                data:['销量','产量']
+            },
+            xAxis : [
+                {
+                    type : 'category',
+//                                    data : ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","手巾"]
+                    data : cols_11
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value'
+                }
+            ],
+            series :
+                series
+            /*  [
+             {
+             "name":"销量",
+             "type":"bar",
+             "data":[5, 20, 40, 10, 10, 20]
+             },
+             {
+             "name":"产量",
+             "type":"bar",
+             "data":[5, 20, 40, 10, 10, 2]
+             }
+             ]*/
+        };
+        console.log("ec22222==="+echarts);
+        console.log("option==="+option.series[0]);
+        console.log("bardemo==="+bardemo);
+        var bar = document.getElementById('bar');
+
+        bardemo.barTU(bar,option);
+
+//       var myChart = echarts.init(document.getElementById('data-table'));
+//        // 为echarts对象加载数据
+//        myChart.setOption(option);
+
+    };
 
 
 
+    //获取二维数组不为null最大I和J值
+    function getMaxIndex(dataArr){
+        var obj = new Object();
+        //获取表格二维数组
+//        var dataArr = hot.getData();
+        console.log(dataArr);
 
+        //获取最大下标
+        var tempArr;
+        obj.maxI = 0;
+        obj.maxJ = 0;
+        for(var i=0;i<dataArr.length;i++){
+            tempArr = dataArr[i];
+            for(var j=0;j<tempArr.length;j++){
+                if(dataArr[i][j] == null){
+
+                }else{
+                    if(i > obj.maxI){
+                        obj.maxI = i;
+                    }
+                    if(j > obj.maxJ){
+                        obj.maxJ = j;
+                    }
+                }
+            }
+
+        }
+//        console.log(hot.getData(maxI,maxJ));
+        return obj;
+    }
 
 
 });
